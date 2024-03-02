9533f47fef78b170a0602d74635e79d6b5ac14f8
Pavankumar Uppar <pavankumar.ai20@bmsce.ac.in>
2024-03-02 22:42:02 +0530

    added api limit to music

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,6 +1,7 @@
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
-import Replicate from "replicate"
+import Replicate from "replicate";
+import { increaseApiLimit, checkApilimit } from '@/lib/api-limit';
 
 
 const  replicate = new Replicate({
@@ -27,6 +28,12 @@ export async function POST(
             return new NextResponse("Prompt are required", { status: 400 })
         }
 
+        const freeTrial = await checkApilimit();
+
+        if(!freeTrial) {
+            return new NextResponse("Free trial has expired.", {status: 403})
+        }
+
         const response = await replicate.run(
             "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
             {
@@ -35,6 +42,8 @@ export async function POST(
               }
             }
           );
+
+        await increaseApiLimit();
 
         return NextResponse.json(response)
 
