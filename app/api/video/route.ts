1c4e474c2d21562bea6c23ab5e2e78650e5d8a85
Pavankumar Uppar <pavankumar.ai20@bmsce.ac.in>
2024-03-02 22:43:26 +0530

    added api limit to video page

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,7 +1,7 @@
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
-import Replicate from "replicate"
-
+import Replicate from "replicate";
+import { increaseApiLimit, checkApilimit } from '@/lib/api-limit';
 
 const  replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN!
@@ -27,6 +27,12 @@ export async function POST(
             return new NextResponse("Prompt are required", { status: 400 })
         }
 
+        const freeTrial = await checkApilimit();
+
+        if(!freeTrial) {
+            return new NextResponse("Free trial has expired.", {status: 403})
+        }
+
         const response = await replicate.run(
             "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
             {
@@ -35,6 +41,7 @@ export async function POST(
               }
             }
           );
+        await increaseApiLimit();
 
         return NextResponse.json(response)
 
